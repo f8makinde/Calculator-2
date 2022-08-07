805dec84bdca0a317e5d538f09a4a136efc4011a
Faith Makinde <faithmakinde0@gmail.com>
2022-08-07 17:55:10 +0100

    bug fixed

--- index.js
+++ index.js
@@ -3,23 +3,35 @@ let sumEl = document.getElementById("sum-el");
 
 function add() {
     let number1 = parseFloat(document.getElementById("num1-el").value) 
-let number2 = parseFloat(document.getElementById("num2-el").value)
+   let number2 = parseFloat(document.getElementById("num2-el").value)
     sumEl.innerHTML = (number1 + number2);
+    if(isNaN(sumEl.innerHTML)){
+        sumEl.innerHTML = "";
+    }
 }
 function subtract(){
     let number1 = parseFloat(document.getElementById("num1-el").value) 
 let number2 = parseFloat(document.getElementById("num2-el").value)
 sumEl.innerHTML = (number1 - number2);
+if(isNaN(sumEl.innerHTML)){
+    sumEl.innerHTML = "";
+}
 }
 function divide(){
     let number1 = parseFloat(document.getElementById("num1-el").value) 
 let number2 = parseFloat(document.getElementById("num2-el").value)
 sumEl.innerHTML = (number1 / number2);
+if(isNaN(sumEl.innerHTML)){
+    sumEl.innerHTML = "";
+}
 }
 function multiply(){
     let number1 = parseFloat(document.getElementById("num1-el").value) 
 let number2 = parseFloat(document.getElementById("num2-el").value)
 sumEl.innerHTML = (number1 * number2);
+if(isNaN(sumEl.innerHTML)){
+    sumEl.innerHTML = "";
+}
 }
 function del(){
 document.getElementById("num1-el").value = "";
